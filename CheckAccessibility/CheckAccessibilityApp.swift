e3ea209fa18283abfe19b40cfb264e4881d09d6b
Leandro Estrada <leandro.a.estrada@gmail.com>
2023-10-05 11:58:41 -0300

    Start repo

---
+++ CheckAccessibility/CheckAccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckAccessibilityApp.swift
+//  CheckAccessibility
+//
+//  Created by Leandro de Araujo Estrada on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckAccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
